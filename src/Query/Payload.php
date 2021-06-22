232185617e7025ab8200e96720ef0192ddd32573
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-06-22 17:17:03 +0500

    get methods with sanitization

--- src/Query/Payload.php
+++ src/Query/Payload.php
@@ -15,6 +15,7 @@ declare(strict_types=1);
 namespace Comely\Http\Query;
 
 use Comely\Buffer\AbstractByteArray;
+use Comely\Utils\ASCII;
 
 /**
  * Class Payload
@@ -88,39 +89,108 @@ class Payload extends AbstractDataIterator
     }
 
     /**
-     * Special method to retrieve values from child arrays (i.e. "user.prop1.prop1b" => $user["prop1"]["prop1b"])
-     * @param string $key
-     * @return string|int|float|array|null
+     * @param string $prop
+     * @return string|int|float|array|bool|null
      */
-    public function find(string $key): string|int|float|array|null
+    public function getUnsafe(string $prop): string|int|float|array|null|bool
     {
-        if (!preg_match('/^[\w\-]+(\.[\w\-]+)+$/i', $key)) {
-            return $this->get($key);
-        }
-
-        $tokens = explode(".", $key);
-        $arr = $this->get($tokens[0]);
-        array_shift($tokens);
-
-        if (!is_array($arr)) {
-            return null;
-        }
-
-        $deep = array_map(function ($prop) {
-            return sprintf('["%s"]', $prop);
-        }, $tokens);
-
-        $eval = "return \$arr" . implode($deep) . " ?? null;";
-        return eval($eval);
+        $prop = $this->getProp($prop);
+        return $prop?->value;
     }
 
     /**
      * @param string $prop
-     * @return string|int|float|array|null
+     * @param string|null $allowLowChars
+     * @param string|null $stripChars
+     * @param bool $trim
+     * @return string
      */
-    public function get(string $prop): string|int|float|array|null
+    public function getASCII(string $prop, ?string $allowLowChars = null, ?string $stripChars = null, bool $trim = true): string
     {
-        $prop = $this->getProp($prop);
-        return $prop?->value;
+        $value = $this->getUnsafe($prop);
+        if (!is_string($value)) {
+            return "";
+        }
+
+        $value = ASCII::Filter($value, $allowLowChars, $stripChars);
+        if ($trim) {
+            $value = trim($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param string $prop
+     * @param string|null $strAllowLowChars
+     * @param bool $strStripHigh
+     * @return string|int|float|array|bool|null
+     */
+    public function getSanitized(string $prop, ?string $strAllowLowChars = null, bool $strStripHigh = true): string|int|float|array|null|bool
+    {
+        return $this->sanitizeValue($this->getUnsafe($prop), $strAllowLowChars, $strStripHigh);
+    }
+
+    /**
+     * @param string $prop
+     * @param bool $unSigned
+     * @return int|null
+     */
+    public function getInt(string $prop, bool $unSigned = false): ?int
+    {
+        $value = $this->getUnsafe($prop);
+        if (is_string($value) && preg_match('/^-?[1-9][0-9]+$/', $value)) {
+            $value = intval($value);
+        }
+
+        if (is_int($value)) {
+            if ($unSigned) {
+                return $value >= 0 ? $value : null;
+            }
+
+            return $value;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param $in
+     * @param string|null $strAllowLowChars
+     * @param bool $strStripHigh
+     * @return string|int|float|array|bool|null
+     */
+    public function sanitizeValue($in, ?string $strAllowLowChars = null, bool $strStripHigh = true): string|int|float|array|null|bool
+    {
+        if (is_string($in)) {
+            if (!$strStripHigh) {
+                return filter_var($in, FILTER_UNSAFE_RAW, FILTER_FLAG_STRIP_LOW);
+            }
+
+            return trim(ASCII::Filter($in, allowLowChars: $strAllowLowChars));
+        }
+
+        if (is_scalar($in) || is_null($in)) {
+            return $in;
+        }
+
+        if (is_array($in)) {
+            $sanitized = [];
+            foreach ($in as $key => $value) {
+                if (!is_string($key) && !is_int($key)) {
+                    continue;
+                }
+
+                if (is_string($key) && !preg_match('/^[\w\-.]+$/', $key)) {
+                    continue;
+                }
+
+                $sanitized[$key] = $this->sanitizeValue($value, $strAllowLowChars, $strStripHigh);
+            }
+
+            return $sanitized;
+        }
+
+        return null;
     }
 }
