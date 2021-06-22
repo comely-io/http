ad87fd256782a06e89eddd91a8fa05bd780be4a6
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-06-22 13:24:02 +0500

    forwardable requests

--- src/Router/AbstractController.php
+++ src/Router/AbstractController.php
@@ -14,12 +14,12 @@ declare(strict_types=1);
 
 namespace Comely\Http\Router;
 
+use Comely\Http\Exception\ControllerException;
 use Comely\Http\Exception\RouterException;
 use Comely\Http\Query\Payload;
 use Comely\Http\Request;
 use Comely\Http\Response\ControllerResponse;
 use Comely\Http\Router;
-use Comely\Utils\OOP\OOP;
 use Comely\Utils\OOP\Traits\NotCloneableTrait;
 use Comely\Utils\OOP\Traits\NotSerializableTrait;
 
@@ -35,6 +35,8 @@ abstract class AbstractController
     private Request $request;
     /** @var ControllerResponse */
     private ControllerResponse $response;
+    /** @var string|null */
+    protected ?string $entryPoint = null;
 
     use NotCloneableTrait;
     use NotSerializableTrait;
@@ -43,13 +45,26 @@ abstract class AbstractController
      * AbstractController constructor.
      * @param Router $router
      * @param Request $req
+     * @param AbstractController|null $prev
+     * @param string|null $entryPoint
+     * @throws ControllerException
      */
-    public function __construct(Router $router, Request $req)
+    public function __construct(Router $router, Request $req, ?AbstractController $prev = null, ?string $entryPoint = null)
     {
         $this->router = $router;
         $this->request = $req;
-        $this->response = new ControllerResponse();
+        $this->response = $prev?->response() ?? new ControllerResponse();
 
+        if ($entryPoint) {
+            $this->entryPoint = method_exists($this, $entryPoint) ? $entryPoint : null;
+            if (!$this->entryPoint) {
+                throw new ControllerException(
+                    sprintf('Entrypoint method "%s" does not exist in controller class "%s"', $entryPoint, get_called_class())
+                );
+            }
+        }
+
+        // Callback method will determine and call entrypoint method, prepare response structures, etc...
         $this->callback();
     }
 
@@ -107,42 +122,43 @@ abstract class AbstractController
     }
 
     /**
-     * @param string $pathOrController
+     * @param string $controllerClass
+     * @param string $entryPoint
+     * @return AbstractController
+     * @throws ControllerException
+     */
+    public function forwardToController(string $controllerClass, string $entryPoint): AbstractController
+    {
+        if (!class_exists($controllerClass)) {
+            throw new ControllerException('Cannot forward request; Controller class does not exist');
+        }
+
+        try {
+            $reflect = new \ReflectionClass($controllerClass);
+            if (!$reflect->isSubclassOf('Comely\Http\Router\AbstractController')) {
+                throw new ControllerException(
+                    'Forwarded to controller class does not extend "Comely\Http\Router\AbstractController"'
+                );
+            }
+        } catch (\ReflectionException) {
+            throw new ControllerException('Could not get reflection instance for next controller class');
+        }
+
+        return new $controllerClass($this->router, $this->request, $this, $entryPoint);
+    }
+
+    /**
+     * @param string $path
      * @param string|null $method
      * @param bool|null $bypassHttpAuth
      * @return AbstractController
      * @throws RouterException
      * @throws \Comely\Http\Exception\HttpRequestException
-     * @throws \ReflectionException
      */
-    public function forward(string $pathOrController, ?string $method = null, ?bool $bypassHttpAuth = true): AbstractController
+    public function forward(string $path, ?string $method = null, ?bool $bypassHttpAuth = true): AbstractController
     {
-        // Forward Request directly to a Controller
-        if (OOP::isValidClassName($pathOrController)) {
-            if ($method) {
-                throw new RouterException(
-                    'Second argument not accepted if forwarding directly to a controller class'
-                );
-            }
-
-            if (!class_exists($pathOrController)) {
-                throw new RouterException(
-                    sprintf('Cannot forward request to "%s", class does not exist', $pathOrController)
-                );
-            }
-
-            $reflect = new \ReflectionClass($pathOrController);
-            if (!$reflect->isSubclassOf('Comely\Http\Router\AbstractController')) {
-                throw new RouterException(
-                    'Forwarded to controller class does not extend "Comely\Http\Router\AbstractController"'
-                );
-            }
-
-            return $pathOrController($this->router, $this->request);
-        }
-
         // Create new Request
-        $req = new Request($method ?? $this->request->method(), $pathOrController);
+        $req = new Request($method ?? $this->request->method(), $path);
         $req->override(
             clone $this->request->headers(),
             clone $this->request->payload(),
